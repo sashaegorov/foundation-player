05d1bb9f309e6cf7ba1dcc674e313add854d6757
is_it_rita <candor.cane@gmail.com>
2015-11-11 12:32:28 +0400

    player initialization tests

--- spec/javascripts/dom_spec.js
+++ spec/javascripts/dom_spec.js
@@ -1,14 +1,16 @@
-describe("A suite", function() {
+describe("Initialized player", function() {
   beforeEach(function() {
-    jasmine.getFixtures().load('no-1.html')
+    jasmine.getFixtures().load('player.html')
     $('.no-1').foundationPlayer()
-    no1 = $('.no-1').data('FoundationPlayer');
+    player = $('.no-1').data('FoundationPlayer');
   });
 
   it("Initialized player is in DOM", function() {
-    expect(no1).toBeDefined();
+    expect(player).toBeDefined();
   });
   it("player has default size", function() {
-    expect(no1.options.size).toEqual('normal');
+    expect(player.options.size).toEqual('normal');
   });
+  
+
 });
